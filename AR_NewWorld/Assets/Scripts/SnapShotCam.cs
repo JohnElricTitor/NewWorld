a32d90766a172d33cc4d5c1da5d7eec2cda7f412
JohnTitor <32987636+JohnElricTitor@users.noreply.github.com>
2021-11-29 16:10:45 -0500

    Implemented 3 butterflies and some cam features
    Camera takes photos.
    
    Bugs
    -Gallery button doesn't work
    -landscape images come put with white space
    -Recording doesnt work
    -Rendered images come out too dark

--- AR_NewWorld/Assets/Scripts/SnapShotCam.cs
+++ AR_NewWorld/Assets/Scripts/SnapShotCam.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Camera))]
 public class SnapShotCam : MonoBehaviour
 {
-    Camera snapShotCam;
+    public GameObject[] cameras; //Left, Right, Portrait
     int resWidth = 256;
     int resHeight = 256;
 
+    int currentCam;
+
     private void Awake()
     {
-        snapShotCam = GetComponent<Camera>();
-        if (snapShotCam.targetTexture == null)
-            snapShotCam.targetTexture = new RenderTexture(resWidth, resHeight, 24);
-        else
-        {
-            resWidth = snapShotCam.targetTexture.width;
-            resHeight = snapShotCam.targetTexture.height;
-        }
-        //snapCam.gameObject.SetActive(false);
+        cameras[1].SetActive(true);
+        resWidth = cameras[1].GetComponent<Camera>().targetTexture.width;
+        resHeight = cameras[1].GetComponent<Camera>().targetTexture.height;
     }
-
+    private void Update()
+    {
+        Debug.Log(Input.deviceOrientation);
+       // CamOrientation();
+        
+    }
     public void Capture()
     {
+        CamOrientation();
         StartCoroutine(TakeScreenshotAndSave());
     }
 
@@ -32,13 +33,12 @@ public class SnapShotCam : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        Texture2D ss = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
-        RenderTexture.active = snapShotCam.targetTexture;
-        ss.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        RenderTexture.active = cameras[currentCam].GetComponent<Camera>().targetTexture;
+        Texture2D ss = new Texture2D(RenderTexture.active.width, RenderTexture.active.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, RenderTexture.active.width, RenderTexture.active.height), 0, 0);
         ss.Apply();
         // Save the screenshot to Gallery/Photos
         NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
-
 
         Debug.Log("Permission result: " + permission);
 
@@ -46,8 +46,39 @@ public class SnapShotCam : MonoBehaviour
         Destroy(ss);
     }
 
-    public void Record()
+    private void CamOrientation()
     {
+        foreach(GameObject cam in cameras)
+        {
+            cam.SetActive(false);
+        }
 
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            cameras[0].SetActive(true);
+            currentCam = 0;
+        }
+
+        else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        {
+            cameras[2].SetActive(true);
+            currentCam = 2;
+        }
+        else
+        {
+            cameras[1].SetActive(true);
+            currentCam = 1;
+        }
+
+    }
+
+    public void StartRecording()
+    {
+        //cameras[currentCam].GetComponent<Camera>().r
+    }
+
+    public void StopRecording()
+    {
+        //NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(ss, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
     }
 }

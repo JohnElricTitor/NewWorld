93e5cedf5c4e7e6690014fc6aa97c4c9c21a4f56
JohnTitor <32987636+JohnElricTitor@users.noreply.github.com>
2021-12-04 04:13:43 -0500

    Camera Capture Fixed
    Unity : 2020.3.0f1
    -Camera view now uses post processing to control look.
    -Camera capture fixed. Uses 3 different cameras to correct the orientation. Uses same post processing as main camera to maintain photo look. FOV being used in all 3 cameras to fix white space issues.

--- AR_NewWorld/Assets/Scripts/SnapShotCam.cs
+++ AR_NewWorld/Assets/Scripts/SnapShotCam.cs
@@ -1,28 +1,16 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 public class SnapShotCam : MonoBehaviour
 {
-    public GameObject[] cameras; //Left, Right, Portrait
+    public Camera[] cameras; //Left, Right, Portrait
     int resWidth = 256;
     int resHeight = 256;
 
-    int currentCam;
+    [HideInInspector] public int currentCam;
+    
 
-    private void Awake()
-    {
-        cameras[1].SetActive(true);
-        resWidth = cameras[1].GetComponent<Camera>().targetTexture.width;
-        resHeight = cameras[1].GetComponent<Camera>().targetTexture.height;
-    }
-    private void Update()
-    {
-        Debug.Log(Input.deviceOrientation);
-       // CamOrientation();
-        
-    }
     public void Capture()
     {
         CamOrientation();
@@ -32,53 +20,48 @@ public class SnapShotCam : MonoBehaviour
     private IEnumerator TakeScreenshotAndSave()
     {
         yield return new WaitForEndOfFrame();
+        resWidth = cameras[currentCam].targetTexture.width;
+        resHeight = cameras[currentCam].targetTexture.height;
 
-        RenderTexture.active = cameras[currentCam].GetComponent<Camera>().targetTexture;
-        Texture2D ss = new Texture2D(RenderTexture.active.width, RenderTexture.active.height, TextureFormat.RGB24, false);
-        ss.ReadPixels(new Rect(0, 0, RenderTexture.active.width, RenderTexture.active.height), 0, 0);
-        ss.Apply();
+        RenderTexture tempRT = new RenderTexture(resWidth, resHeight, 24);
+        cameras[currentCam].targetTexture = tempRT;
+        cameras[currentCam].Render();
+
+        RenderTexture.active = tempRT;
+        Texture2D capture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        capture.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+
         // Save the screenshot to Gallery/Photos
-        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(capture, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
 
         Debug.Log("Permission result: " + permission);
 
         // To avoid memory leaks
-        Destroy(ss);
+        Destroy(capture);
     }
 
     private void CamOrientation()
     {
-        foreach(GameObject cam in cameras)
+        foreach (Camera cam in cameras)
         {
-            cam.SetActive(false);
+            cam.gameObject.SetActive(false);
         }
 
-        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft)         //LEFT
         {
-            cameras[0].SetActive(true);
+            cameras[0].gameObject.SetActive(true);
             currentCam = 0;
         }
 
-        else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        else if (Input.deviceOrientation == DeviceOrientation.LandscapeRight)   //RIGHT
         {
-            cameras[2].SetActive(true);
-            currentCam = 2;
-        }
-        else
-        {
-            cameras[1].SetActive(true);
+            cameras[1].gameObject.SetActive(true);
             currentCam = 1;
         }
-
-    }
-
-    public void StartRecording()
-    {
-        //cameras[currentCam].GetComponent<Camera>().r
-    }
-
-    public void StopRecording()
-    {
-        //NativeGallery.Permission permission = NativeGallery.SaveVideoToGallery(ss, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+        else                                                                    //PORTRAIT
+        {
+            cameras[2].gameObject.SetActive(true);
+            currentCam = 2;
+        }
     }
 }

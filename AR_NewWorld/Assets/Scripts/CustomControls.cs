e94b497b6ceb66eb8f821a382eb5971a42376279
JohnTitor <32987636+JohnElricTitor@users.noreply.github.com>
2021-12-14 04:45:06 -0500

    Gallery and new UI
    UI Improvements
    1. Transparent black backgrounds on the buttons
    2. Scaling of buttons fixed
    3. Photo capture has red button swap when clicked
    
    -Gallery function added (horizontal photos still stretch when previewing in app)
    
    -Audio for photo capture added
    
    -Native mobile control bar now visible (still disabled if the phone originally had it off. Native back button still not implemented into functionality)

---
+++ AR_NewWorld/Assets/Scripts/CustomControls.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CustomControls : MonoBehaviour
+{
+    [SerializeField] UnityEvent tester;
+
+
+}
+
--- AR_NewWorld/Assets/Scripts/SnapShotCam.cs
+++ AR_NewWorld/Assets/Scripts/SnapShotCam.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class SnapShotCam : MonoBehaviour
 {
     public Camera[] cameras; //Left, Right, Portrait
     int resWidth = 256;
     int resHeight = 256;
+    [SerializeField] RawImage previewImage;
 
     [HideInInspector] public int currentCam;
-    
 
     public void Capture()
     {
@@ -18,6 +18,29 @@ public class SnapShotCam : MonoBehaviour
     }
 
     private IEnumerator TakeScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+        resWidth = cameras[currentCam].targetTexture.width;
+        resHeight = cameras[currentCam].targetTexture.height;
+
+        RenderTexture tempRT = new RenderTexture(resWidth, resHeight, 24);
+        cameras[currentCam].targetTexture = tempRT;
+        cameras[currentCam].Render();
+
+        RenderTexture.active = tempRT;
+        Texture2D capture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        capture.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+
+        // Save the screenshot to Gallery/Photos
+        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(capture, "GalleryTest", "Image.png", (success, path) => Debug.Log("Media save result: " + success + " " + path));
+
+        Debug.Log("Permission result: " + permission);
+
+        // To avoid memory leaks
+        Destroy(capture);
+    }
+
+    private IEnumerator TakeVideoAndSave()
     {
         yield return new WaitForEndOfFrame();
         resWidth = cameras[currentCam].targetTexture.width;
@@ -64,4 +87,35 @@ public class SnapShotCam : MonoBehaviour
             currentCam = 2;
         }
     }
+
+
+    public void PickImage()
+    {
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>
+        {
+            Debug.Log("Image path: " + path);
+            if (path != null)
+            {
+                // Create Texture from selected image
+                Texture2D texture = NativeGallery.LoadImageAtPath(path);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+
+
+                previewImage.texture = texture;
+                previewImage.transform.parent.gameObject.SetActive(true);
+               
+
+                // If a procedural texture is not destroyed manually, 
+                // it will only be freed after a scene change
+                //Destroy(texture, 5f);
+            }
+        });
+
+        Debug.Log("Permission result: " + permission);
+    }
+
 }
